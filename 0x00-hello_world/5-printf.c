a98e94684d95e4b7aaf8bc91bb84b3d7a6decfc3
ndubuisijosephc <ndubuisijoseph19.com>
2023-07-16 20:54:16 -0700

    make

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,19 @@
- // Fig. 2.1: fig02_01.c
- // A first program in C.
- #include <stdio.h>
- 
- // function main begins program execution
- int main(void)
-printf( "proper grammar, but the outcome is a piece of art,\n" );
-} // end function main
+/*
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
