c73bc14d482825f83e51d4a603de7152c4f63b0f
ndubuisijosephc <ndubuisijoseph19.com>
2023-07-16 20:20:54 -0700

    print c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+ // Fig. 2.1: fig02_01.c
+ // A first program in C.
+ #include <stdio.h>
+ 
+ // function main begins program execution
+ int main(void)
+printf( "proper grammar, but the outcome is a piece of art,\n" );
+} // end function main
